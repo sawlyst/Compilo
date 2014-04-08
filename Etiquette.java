6ec40d7ba65bb2224c192c695dc1b15153c64a43
sawlyst <thommoreau@hotmail.fr>
2014-04-08 16:34:15 +0200

    Delete Etiquette.java

--- Etiquette.java
+++
@@ -1,7 +0,0 @@
-public interface Etiquette {
-
-	public String getEtiquetteFin();
-	public String getEtiquetteDebut();
-	public String getEtiquetteFinAndFinish();
-	public String getEtiquetteSuivante();
-}
